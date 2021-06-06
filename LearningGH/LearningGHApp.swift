22caae7db5e13fbb70b363b54defd02aedb98038
Arad Saebi <arad.saebi@icloud.com>
2021-06-06 12:51:03 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Arad Saebi on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
